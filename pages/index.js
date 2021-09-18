773fae928e9cb79610c1c9f97d0982483e043a60
echizenyayota <virtualmisso30@gmail.com>
2021-09-18 17:55:56 +0900

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-// import { Heading, Page } from "@shopify/polaris";
-
-// const Index = () => (
-//   <Page>
-//     <Heading>Shopify app with Node and React 🎉</Heading>
-//   </Page>
-// );
-
-// export default Index;
-
-
 import {AppProvider, Button} from '@shopify/polaris';
 import React from 'react';
 import ReactDOM from 'react-dom';
